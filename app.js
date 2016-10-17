68542533b94ad8653476ceca19fc19155d70a59f
Juliane Blier <blier.juliane@gmail.com>
2016-10-17 11:00:35 +0200

    Timer à 25minutes fonctionnel

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 (function(){
-	
-
+	var app = {
+		intervalID: null,
+		timer: 1500,
+		init: function(){
+			app.listeners();
+		},
+		listeners: function(){
+			$('#start').on('click', app.start);
+			$('#stop').on('click', app.stop);
+			$('#reset').on('click', app.reset);
+		},
+		updateView: function(){
+			$('#minutes').html(Math.floor(app.timer / 60));
+			$('#seconds').html(app.timer % 60);
+		},
+		start: function(){
+			app.stop();
+			app.intervalID = setInterval(function(){
+				app.timer--;
+				app.updateView();
+			}, 1000);
+		},
+		stop: function(){
+			clearInterval(app.intervalID);
+		},
+		reset: function(){
+			app.timer = 1500;
+		}
+	}
+	app.init();
 })();
