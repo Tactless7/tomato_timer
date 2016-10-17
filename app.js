0b8e04d16889d8c1080fb505706d125f73e14a5c
Juliane Blier <blier.juliane@gmail.com>
2016-10-17 12:07:35 +0200

    Différentes sessions disponibles (travail, pause courte, pause longue)

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 (function(){
 	var app = {
 		intervalID: null,
-		timer: 1500,
+		startTimer: 1500,
+		currentTimer: 1500,
 		init: function(){
 			app.listeners();
 		},
@@ -9,24 +10,46 @@
 			$('#start').on('click', app.start);
 			$('#stop').on('click', app.stop);
 			$('#reset').on('click', app.reset);
+			$('#shortBreak').on('click', app.shortBreak);
+			$('#longBreak').on('click', app.longBreak);
+			$('#tomato').on('click', app.tomato);
 		},
 		updateView: function(){
-			$('#minutes').html(Math.floor(app.timer / 60));
-			$('#seconds').html(app.timer % 60);
+			$('#minutes').html(Math.floor(app.currentTimer / 60));
+			$('#seconds').html(app.currentTimer % 60);
+		},
+		decrement: function(){
+			app.intervalID = setInterval(function(){
+				app.currentTimer--;
+				app.updateView();
+			}, 1000);
+		},
+		updateTimer: function(){
+			app.currentTimer = app.startTimer;
 		},
 		start: function(){
 			app.stop();
-			app.intervalID = setInterval(function(){
-				app.timer--;
-				app.updateView();
-			}, 1000);
+			app.decrement();
 		},
 		stop: function(){
 			clearInterval(app.intervalID);
 		},
 		reset: function(){
-			app.timer = 1500;
+			app.updateTimer();
+			app.updateView();
+		},
+		tomato: function(){
+			app.startTimer = 1500;
+			app.updateTimer();
+			},
+		shortBreak: function(){
+			app.startTimer = 300;
+			app.updateTimer();
+		},
+		longBreak: function(){
+			app.startTimer = 600;
+			app.updateTimer();
 		}
-	}
+	};
 	app.init();
 })();
